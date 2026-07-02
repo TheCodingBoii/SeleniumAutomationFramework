2eab01cf762a6a0df37e9129236beb5213e1999b
Shan Gunarathne <madusoftnet91@gmail.com>
2026-07-02 20:24:23 +0530

    selcommit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+	
+	@Test
+	public void testVAlidLogin() {
+		LoginPage loginPage = new LoginPage(driver); 
+		
+		loginPage.enterUserName("admin@yourstore.com");
+		loginPage.enterPassword("admin");
+		loginPage.clickLogin();
+		
+		System.out.println("Title of this page is : " +driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+		
+	}
+		
+		
+	
+
+}
