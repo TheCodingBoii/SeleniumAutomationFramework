a96ae7178c05c12e741f7cb7e02ca813a73e1ac7
Shan Gunarathne <madusoftnet91@gmail.com>
2026-07-06 19:34:16 +0530

    Added Reports with Screenshot

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,9 +2,17 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+
+import utils.ExtentReportManager;
 import utils.Log;
 
 
@@ -14,6 +22,20 @@ import utils.Log;
 public class BaseTest {
 	
 	protected WebDriver driver;
+	protected static ExtentReports extent;
+	protected ExtentTest test;
+	
+	
+	@BeforeSuite
+	public void setupReport() {
+		extent = ExtentReportManager.getReportInstance();
+	}
+	
+	@AfterSuite
+	public void teardownReport() {
+		extent.flush();
+		
+	}
 	
 	@BeforeMethod
 	public void setUp() {
@@ -26,9 +48,15 @@ public class BaseTest {
 	}
 	
 	@AfterMethod
-	public void tearDown() {
-		
-		if(driver !=null) {
+	public void tearDown(ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE) {
+
+			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "LoginFailure");
+			test.fail("Test Failed.. Check Screenshot",
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		}
+
+		if (driver != null) {
 			Log.info("Closing Browser...");
 			driver.quit();
 		}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest {
@@ -13,18 +14,54 @@ public class LoginTest extends BaseTest {
 	public void testVAlidLogin() {
 		
 		Log.info("Starting login test...");
+		test = ExtentReportManager.createTest("Login Test");
+		
+		test.info("navigating to URL");
+		
 		LoginPage loginPage = new LoginPage(driver); 
 		
 		Log.info("Adding credentials");
+		test.info("Adding Credentials");
 		loginPage.enterUserName("admin@yourstore.com");
 		loginPage.enterPassword("admin");
+		test.info("Clicking on Login button");
 		loginPage.clickLogin();
 		
 		System.out.println("Title of this page is : " +driver.getTitle());
 		Log.info("Verifying page title");
+		test.info("Verifying page title");
 		Assert.assertEquals(driver.getTitle(), "Just a moment...");
 		
+		test.pass("Login Successful");
+		
 	}
+	
+	@Test
+	public void testLoginWithInvalidCredentials() {
+
+		Log.info("Starting login test...");
+		test = ExtentReportManager.createTest("Login Test with Invalid Credentials");
+
+		test.info("Navigating to URL");
+		LoginPage loginPage = new LoginPage(driver);
+
+		Log.info("Adding credentials");
+		test.info("Adding Credentails");
+		loginPage.enterUserName("admin1234@yourstore.com");
+		loginPage.enterPassword("admin123");
+		test.info("Clicking on Login button");
+		loginPage.clickLogin();
+
+		System.out.println("Title of the page is : " + driver.getTitle());
+		Log.info("Verifying page title");
+		test.info("Verifying page title");
+		Assert.assertEquals(driver.getTitle(), "Just a moment...123");
+
+		test.pass("Login Successful");
+
+	}
+	
+	
 		
 		
 	
